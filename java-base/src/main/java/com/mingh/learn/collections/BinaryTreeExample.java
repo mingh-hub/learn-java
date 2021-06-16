c5293337fea14b3f00fc78076d6e981bfdf60479
minghai <mingh_sdc@163.com>
2021-06-16 20:53:26 +0800

    BinaryTree: 二叉树实现

---
+++ java-base/src/main/java/com/mingh/learn/collections/BinaryTreeExample.java
@@ -0,0 +1,23 @@
+package com.mingh.learn.collections;
+
+/**
+ * @ClassName BinaryTreeExample
+ * @Author Hai.Ming
+ * @Date 2021/6/16 20:40
+ * @Description 二叉树
+ */
+public class BinaryTreeExample {
+
+    /**
+     * @Author Hai.Ming
+     * @Date 2021/6/16 20:41
+     * @Description 节点
+     **/
+    static class Node<T extends Comparable<T>>{
+        /**
+         * 数据
+         */
+        T data;
+    }
+
+}
